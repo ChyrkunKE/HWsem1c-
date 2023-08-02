088f074ecc81758363f6af6247cb8c6e0f16192e
Katsiara <katsiara@mail.ru>
2023-08-03 00:20:11 +0300

    Test

--- task6/Program.cs
+++ task6/Program.cs
@@ -4,11 +4,11 @@
 // 7 -> нет
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-if(number%2 == 0)
+if (number % 2 == 0)
 {
-    Console.WriteLine ("Это чётное число");
+    Console.WriteLine("Это чётное число");
 }
 else
 {
-    Console.WriteLine ("Это нечётное число");
+    Console.WriteLine("Это нечётное число");
 }
